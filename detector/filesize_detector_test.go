5c082dd7931002791168b77ed9c832afdbb10d98
Manuel Paul Joseph <jmpak80@gmail.com>
2016-01-21 14:27:01 +1100

    added file size detector - currently set default to 1MB

---
+++ detector/filesize_detector_test.go
@@ -0,0 +1,33 @@
+package detector
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/thoughtworks/talisman/git_repo"
+)
+
+func TestShouldFlagLargeFiles(t *testing.T) {
+	results := NewDetectionResults()
+	content := []byte("more than one byte")
+	additions := []git_repo.Addition{git_repo.NewAddition("filename", content)}
+	NewFileSizeDetector(2).Test(additions, NewIgnores(), results)
+	assert.True(t, results.HasFailures(), "Expected file to fail the check against file size detector.")
+}
+
+func TestShouldNotFlagSmallFiles(t *testing.T) {
+	results := NewDetectionResults()
+	content := []byte("m")
+	additions := []git_repo.Addition{git_repo.NewAddition("filename", content)}
+	NewFileSizeDetector(2).Test(additions, NewIgnores(), results)
+	assert.False(t, results.HasFailures(), "Expected file to not to fail the check against file size detector.")
+}
+
+func TestShouldNotFlagIgnoredLargeFiles(t *testing.T) {
+	results := NewDetectionResults()
+	content := []byte("more than one byte")
+	filename := "filename"
+	additions := []git_repo.Addition{git_repo.NewAddition(filename, content)}
+	NewFileSizeDetector(2).Test(additions, NewIgnores(filename), results)
+	assert.True(t, results.Successful(), "expected file %s to be ignored by file size detector", filename)
+}
