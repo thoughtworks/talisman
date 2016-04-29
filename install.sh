bfe82f74d77265b72f089e272e330212e5b6efcb
Jack Singleton <git@jacksingleton.com>
2016-04-28 17:33:20 -0700

    configure template directory in gitconfig instead of installing to system wide templates

--- install.sh
+++ install.sh
@@ -10,8 +10,9 @@ run() {
   GITHUB_URL="https://github.com/thoughtworks/talisman"
   BINARY_URL="$GITHUB_URL/releases/download/v0.1.0/talisman"
   EXPECTED_BINARY_SHA="fdfa31d22e5acaef3ca2f57b1036f4c2f3b9601b00524c753a5919a6c8fa3cd3"
-  PRE_PUSH_HOOK=".git/hooks/pre-push"
+  REPO_PRE_PUSH_HOOK=".git/hooks/pre-push"
   DOWNLOADED_BINARY=""
+  DEFAULT_GLOBAL_TEMPLATE_DIR="$HOME/.git-templates"
 
   echo_error() {
     echo -ne "\e[31m" >&2
@@ -42,78 +43,72 @@ run() {
   }
 
   install_to_repo() {
-    if [ -x "$PRE_PUSH_HOOK" ]; then
+    if [ -x "$REPO_PRE_PUSH_HOOK" ]; then
       echo
-      echo_error "Oops, it looks like you already have a pre-push hook installed at '$PRE_PUSH_HOOK'"
-      echo_error "Talisman is not compatible with other hooks right now, sorry"
+      echo_error "Oops, it looks like you already have a pre-push hook installed at '$REPO_PRE_PUSH_HOOK'."
+      echo_error "Talisman is not compatible with other hooks right now, sorry."
       echo_error "If this is a problem for you, please open an issue: https://github.com/thoughtworks/talisman/issues/new"
       exit 2
     fi
 
     download_and_verify
 
-    cp $DOWNLOADED_BINARY $PRE_PUSH_HOOK
-    chmod +x $PRE_PUSH_HOOK
+    cp $DOWNLOADED_BINARY $REPO_PRE_PUSH_HOOK
+    chmod +x $REPO_PRE_PUSH_HOOK
 
     echo
     echo -ne "\e[32m"
-    echo "Talisman successfully installed to '$PRE_PUSH_HOOK'"
+    echo "Talisman successfully installed to '$REPO_PRE_PUSH_HOOK'"
     echo -ne "\e[0m"
   }
 
   install_to_git_templates() {
 
     if [ ! -t 1 ]; then
-      echo_error "Headless install to system templates is not supported"
+      echo_error "Headless install to system templates is not supported."
       echo_error "If you would like this feature, please open an issue: https://github.com/thoughtworks/talisman/issues/new"
       exit 6
     fi
 
-    echo
-    echo "Talisman can be installed to your system git hook"
-    echo "templates. It will then be automatically set up in any git"
-    echo "repository that you 'init' or 'clone'."
-    echo
-    echo "This script will inform you of every file it is going to add"
-    echo "to your system, and give you a chance to back out."
+    TEMPLATE_DIR=$(git config --global init.templatedir) || true
 
-    echo
-    read -u1 -p "Install Talisman to your system git hook templates? (Y/n) " INSTALL
-
-    if [ "$INSTALL" != "Y" ] && [ "$INSTALL" != "y" ] && [ "$INSTALL" != "" ]; then
+    if [ "$TEMPLATE_DIR" = "" ]; then
+      echo "No git template directory is configured. Let's add one."
+      echo "(this will override any system git templates and modify your git config file)"
       echo
-      echo_error "Not installing Talisman"
-      echo_error "If you were trying to install into a single git repo, re-run this command from that repo"
-      echo_error "You can always download/compile manually from our Github page: $GITHUB_URL"
-      exit 4
+      read -u1 -p "Git template directory: ($DEFAULT_GLOBAL_TEMPLATE_DIR) " TEMPLATE_DIR
+      TEMPLATE_DIR=${TEMPLATE_DIR:-$DEFAULT_GLOBAL_TEMPLATE_DIR}
+      git config --global init.templatedir $TEMPLATE_DIR
+    else
+      echo "You already have a git template directory configured."
+      read -u1 -p "Add Talisman to '$TEMPLATE_DIR/hooks?' (Y/n) " USE_EXISTING
+      
+      if [ "$USE_EXISTING" != "Y" ] && [ "$USE_EXISTING" != "y" ] && [ "$USE_EXISTING" != "" ]; then
+	echo
+	echo_error "Not installing Talisman."
+	echo_error "If you were trying to install into a single git repo, re-run this command from that repo."
+	echo_error "You can always download/compile manually from our Github page: $GITHUB_URL"
+	exit 4
+      fi
     fi
 
-    echo
-    echo "Searching for your git template directories..."
-    GIT_HOOK_TEMPLATE_DIRS=$(find /usr/share /usr/local /opt /Library /Applications -path '*/git-core/templates/hooks' 2>/dev/null) || true
-    
-    echo
-    echo "Installing Talisman as a 'pre-push' binary in the following locations:"
-    echo
-    while read -r D; do echo $D; done <<< "$GIT_HOOK_TEMPLATE_DIRS"
-    echo
-    read -u1 -p "Continue? (Y/n) " CONTINUE
-    
-    if [ "$CONTINUE" != "Y" ] && [ "$CONTINUE" != "y" ] && [ "$CONTINUE" != "" ]; then
+    # Support '~' in path
+    TEMPLATE_DIR=${TEMPLATE_DIR/#\~/$HOME}
+
+    if [ -f "$TEMPLATE_DIR/hooks/pre-push" ]; then
       echo
-      echo_error "Not installing Talisman"
-      echo_error "You can always download/compile manually from our Github page: $GITHUB_URL"
+      echo_error "Oops, it looks like you already have a pre-push hook installed at '$TEMPLATE_DIR/hooks/pre-push'."
+      echo_error "Talisman is not compatible with other hooks right now, sorry."
+      echo_error "If this is a problem for you, please open an issue: https://github.com/thoughtworks/talisman/issues/new"
       exit 5
     fi
+    
+    mkdir -p "$TEMPLATE_DIR/hooks"
 
     download_and_verify
 
-    echo
-    echo "Installing Talisman to the above locations (may ask for sudo access)"
-    while read -r DIR; do
-      sudo cp $DOWNLOADED_BINARY "$DIR/pre-push"
-      sudo chmod +x "$DIR/pre-push"
-    done <<< "$GIT_HOOK_TEMPLATE_DIRS"
+    cp $DOWNLOADED_BINARY "$TEMPLATE_DIR/hooks/pre-push"
+    chmod +x "$TEMPLATE_DIR/hooks/pre-push"
     
     echo
     echo -ne "\e[32m"
