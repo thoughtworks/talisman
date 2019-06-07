a2eb67dbdf4d69ca7a8238c2a0c806cb0c36aaf0
Jaydeepc <jaydeepc@in-Jaydeepc.local>
2019-06-07 19:11:37 +0530

    Worked on review comments: ** Added necessary info when Talisman Html Report is not installed. ** Changed --htmlreport to --scanWithHtml ** Moved directory copy code from report.go to utility.go ** Changed html-report-directory to .talisman/ from .talisman/bin

--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module talisman
 require (
 	github.com/Sirupsen/logrus v0.0.0-20151204141443-446d1c146faa
 	github.com/bmatcuk/doublestar v1.1.1
+	github.com/common-nighthawk/go-figure v0.0.0-20190529165535-67e0ed34491a
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/drhodes/golorem v0.0.0-20120624033213-6e38d8d5e455
+	github.com/fatih/color v1.7.0
+	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-runewidth v0.0.4 // indirect
 	github.com/mitchellh/gox v0.4.0 // indirect
 	github.com/mitchellh/iochan v1.0.0 // indirect
@@ -12,6 +15,5 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.3
 	github.com/stretchr/testify v0.0.0-20151208002404-e3a8ff8ce365
-	golang.org/x/sys v0.0.0-20181031143558-9b800f95dbbc // indirect
 	gopkg.in/yaml.v2 v2.2.1
 )
--- report/report.go
+++ report/report.go
@@ -3,20 +3,21 @@ package report
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/user"
-	"path"
 	"path/filepath"
 	"talisman/detector"
+	"talisman/utility"
+
+	"github.com/fatih/color"
 )
 
 var reportsFolder string
 
 const htmlFileName string = "report.html"
 const jsonFileName string = "report.json"
+const htmlReportDir string = "talisman_html_report"
 
 // GenerateReport generates a talisman scan report in html format
 func GenerateReport(r *detector.DetectionResults, directory string) string {
@@ -25,32 +26,33 @@ func GenerateReport(r *detector.DetectionResults, directory string) string {
 	var jsonFilePath string
 	var home_dir string
 	var base_report_dir_path string
-	const htmlReportDir string = "talisman_html_report"
-
-	if directory == htmlReportDir {
-		path = directory
-		// base_report_dir_path = filepath.Join(home_dir, ".talisman/bin/talisman_html_report")
-		// Dir(base_report_dir_path, "talisman_html_report")
-	} else {
-		path = filepath.Join(directory, "talisman_reports")
-	}
-
-	jsonFilePath = filepath.Join(path, "/data", jsonFileName)
 
 	usr, err := user.Current()
 	home_dir = usr.HomeDir
 
-	base_report_dir_path = filepath.Join(home_dir, ".talisman", "bin", htmlReportDir)
+	if directory == htmlReportDir {
+		path = directory
+		base_report_dir_path = filepath.Join(home_dir, ".talisman", htmlReportDir)
+		jsonFilePath = filepath.Join(path, "/data", jsonFileName)
+		os.MkdirAll(path, 0755)
+		err = utility.Dir(base_report_dir_path, htmlReportDir)
+		if err != nil {
+			generateErrorMsg()
+		}
+	} else {
+		path = filepath.Join(directory, "talisman_reports", "/data")
+		jsonFilePath = filepath.Join(path, jsonFileName)
+	}
 
 	os.MkdirAll(path, 0755)
-	Dir(base_report_dir_path, htmlReportDir)
 
 	jsonFile, err := os.Create(jsonFilePath)
-	if err != nil {
-		log.Fatal("Cannot create report.json file", err)
 
+	if err != nil {
+		fmt.Println("\n")
+		log.Fatal("Cannot create report.json file\n", err)
 	}
-	//jsonResultSchema := detector.GetJsonSchema(r)
+
 	jsonString, err := json.Marshal(r)
 	if err != nil {
 		log.Fatal("Unable to marshal JSON")
@@ -60,61 +62,15 @@ func GenerateReport(r *detector.DetectionResults, directory string) string {
 	return path
 }
 
-func File(src, dst string) error {
-	var err error
-	var srcfd *os.File
-	var dstfd *os.File
-	var srcinfo os.FileInfo
-
-	if srcfd, err = os.Open(src); err != nil {
-		return err
-	}
-	defer srcfd.Close()
-
-	if dstfd, err = os.Create(dst); err != nil {
-		return err
-	}
-	defer dstfd.Close()
-
-	if _, err = io.Copy(dstfd, srcfd); err != nil {
-		return err
-	}
-	if srcinfo, err = os.Stat(src); err != nil {
-		return err
-	}
-	return os.Chmod(dst, srcinfo.Mode())
-}
-
-func Dir(src string, dst string) error {
-
-	var err error
-	var fds []os.FileInfo
-	var srcinfo os.FileInfo
-
-	if srcinfo, err = os.Stat(src); err != nil {
-		return err
-	}
-
-	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
-		return err
-	}
-
-	if fds, err = ioutil.ReadDir(src); err != nil {
-		return err
-	}
-	for _, fd := range fds {
-		srcfp := path.Join(src, fd.Name())
-		dstfp := path.Join(dst, fd.Name())
-
-		if fd.IsDir() {
-			if err = Dir(srcfp, dstfp); err != nil {
-				fmt.Println(err)
-			}
-		} else {
-			if err = File(srcfp, dstfp); err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
-	return nil
+func generateErrorMsg() {
+	color.HiMagenta("\nLooks like you are using 'talisman --htmlreport' for scanning.")
+	color.HiMagenta("But it appears that you have not installed Talisman Html Report")
+	color.HiMagenta("Please go through Talisman Readme and make sure you install the same from:")
+	color.Yellow("\nhttps://github.com/jaydeepc/talisman-html-report")
+	color.Cyan("\nOR use 'talisman --scan' if you want the JSON report alone\n")
+	fmt.Println("\n")
+	color.Red("Failed: Unable to perform Scan")
+	fmt.Println("\n")
+	log.Fatalln("Run Status: Failed\n")
+	fmt.Println("\n")
 }
