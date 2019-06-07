2f8c3e9ba3621fbc16d0d055e9c81c1f8ca6385a
Jaydeep Chakrabarty <jaydeepc@thoughtworks.com>
2019-06-07 19:11:37 +0530

    Worked on review comments: ** Added necessary info when Talisman Html Report is not installed. ** Changed --htmlreport to --scanWithHtml ** Moved directory copy code from report.go to utility.go ** Changed html-report-directory to .talisman/ from .talisman/bin
    Committer: Jaydeep Chakrabarty <jaydeepc@thoughtworks.com>
    Author:    Jaydeep Chakrabarty <jaydeepc@thoughtworks.com>

--- utility/utility.go
+++ utility/utility.go
@@ -3,7 +3,13 @@ package utility
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"io"
 	"io/ioutil"
+	"os"
+	"path"
+
+	figure "github.com/common-nighthawk/go-figure"
 )
 
 //UniqueItems returns the array of strings containing unique items
@@ -41,4 +47,71 @@ func hashByte(contentPtr *[]byte) string {
 	hasher := sha256.New()
 	hasher.Write(contents)
 	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+//Creates art for console output
+func CreateArt(msg string) {
+	myFigure := figure.NewFigure(msg, "basic", true)
+	myFigure.Print()
+}
+
+//Copies Files and Directories from source to destination
+func File(src, dst string) error {
+	var err error
+	var srcfd *os.File
+	var dstfd *os.File
+	var srcinfo os.FileInfo
+
+	if srcfd, err = os.Open(src); err != nil {
+		return err
+	}
+	defer srcfd.Close()
+
+	if dstfd, err = os.Create(dst); err != nil {
+		return err
+	}
+	defer dstfd.Close()
+
+	if _, err = io.Copy(dstfd, srcfd); err != nil {
+		return err
+	}
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+	}
+	return os.Chmod(dst, srcinfo.Mode())
+}
+
+func Dir(src string, dst string) error {
+
+	var err error
+	var fds []os.FileInfo
+	var srcinfo os.FileInfo
+
+	if srcinfo, err = os.Stat(src); err != nil {
+		return err
+
+	}
+
+	if err = os.MkdirAll(dst, srcinfo.Mode()); err != nil {
+		return err
+	}
+
+	if fds, err = ioutil.ReadDir(src); err != nil {
+		return err
+	}
+	for _, fd := range fds {
+		srcfp := path.Join(src, fd.Name())
+		dstfp := path.Join(dst, fd.Name())
+
+		if fd.IsDir() {
+			if err = Dir(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			if err = File(srcfp, dstfp); err != nil {
+				fmt.Println(err)
+			}
+		}
+	}
+	return nil
 }
