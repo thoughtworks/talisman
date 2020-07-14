58a273b45d68103fe9e193faaf7e5cb464ad2d46
jayandran <jaidg2012@gmail.com>
2020-07-14 20:25:29 +0530

    Add testing coverage for checksumcalculator suggestTalismanRC
    Co-authored-by: Sakthivel B <sakthi836800@gmail.com>

--- checksumcalculator/checksumcalculator_test.go
+++ checksumcalculator/checksumcalculator_test.go
@@ -42,3 +42,45 @@ func TestNewChecksumCalculator(t *testing.T) {
 		assert.Equal(t, expectedCC, actualCC)
 	})
 }
+
+func TestDefaultChecksumCalculator_SuggestTalismanRC(t *testing.T) {
+	t.Run("should return no suggestion for .talismanrc format when no matching file name patterns is sent", func(t *testing.T) {
+		defaultSHA256Hasher := utility.DefaultSHA256Hasher{}
+		gitAdditions := []gitrepo.Addition{
+			{
+				Path: "GitRepoPath1",
+				Name: "GitRepoName1",
+			}, {
+				Path: "GitRepoPath2",
+				Name: "GitRepoName2",
+			},
+		}
+		expectedCC := ""
+		fileNamePatterns := []string{"*NonExistenceFileNamePattern1", "*NonExistenceFileNamePattern2", "*NonExistenceFileNamePattern3"}
+		cc := NewChecksumCalculator(defaultSHA256Hasher, gitAdditions)
+
+		actualCC := cc.SuggestTalismanRC(fileNamePatterns)
+
+		assert.Equal(t, expectedCC, actualCC)
+	})
+
+	t.Run("should return suggestion for .talismanrc format when matching file name patterns is sent", func(t *testing.T) {
+		defaultSHA256Hasher := utility.DefaultSHA256Hasher{}
+		gitAdditions := []gitrepo.Addition{
+			{
+				Path: "GitRepoPath1",
+				Name: "GitRepoName1",
+			}, {
+				Path: "GitRepoPath2",
+				Name: "GitRepoName2",
+			},
+		}
+		expectedCC := "\n\x1b[33m.talismanrc format for given file names / patterns\x1b[0m\nfileignoreconfig:\n- filename: '*1'\n  checksum: 54bbf09e5c906e2d7cc0808729f8120cfa3c4bad3fb6a85689ae23ca00e5a3c8\n- filename: Git*2\n  checksum: d2103425cbcc10118556d7cd63dd198b3e771bcc0359d6b196ddafcf5b87dac5\n"
+		fileNamePatterns := []string{"*1", "Git*2", "*NonExistenceFileNamePattern3"}
+		cc := NewChecksumCalculator(defaultSHA256Hasher, gitAdditions)
+
+		actualCC := cc.SuggestTalismanRC(fileNamePatterns)
+
+		assert.Equal(t, expectedCC, actualCC)
+	})
+}
