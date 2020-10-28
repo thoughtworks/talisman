934c9a145515c516b2c6745bdb6c5e86a28b5cb5
Dinesh B <dineshudt17@gmail.com>
2020-10-28 14:37:49 +0530

    Formatting the Progress bar (#268)

--- detector/chain.go
+++ detector/chain.go
@@ -55,11 +55,58 @@ func (dc *Chain) Test(currentAdditions []gitrepo.Addition, talismanRC *talismanr
 	log.Printf("Number of files to scan: %d\n", len(currentAdditions))
 	log.Printf("Number of detectors: %d\n", len(dc.detectors))
 	total := len(currentAdditions) * len(dc.detectors)
-	bar := pb.StartNew(total)
+	var progressBar = getProgressBar()
+	progressBar.Start(total)
 	for _, v := range dc.detectors {
 		v.Test(cc, currentAdditions, talismanRC, result, func() {
-			bar.Increment()
+			progressBar.Increment()
 		})
 	}
-	bar.Finish()
+	progressBar.Finish()
+}
+
+func getProgressBar() progressBar {
+	if isTerminal() {
+		return &defaultProgressBar{}
+	} else {
+		return &noOpProgressBar{}
+	}
+}
+
+func isTerminal() bool {
+	fileInfo, _ := os.Stdout.Stat()
+	return (fileInfo.Mode() & os.ModeCharDevice) != 0
+}
+
+type progressBar interface {
+	Start(int)
+	Increment()
+	Finish()
+}
+
+type noOpProgressBar struct {
+}
+
+func (d *noOpProgressBar) Start(int) {}
+
+func (d *noOpProgressBar) Increment() {}
+
+func (d *noOpProgressBar) Finish() {}
+
+type defaultProgressBar struct {
+	bar *pb.ProgressBar
+}
+
+func (d *defaultProgressBar) Start(total int) {
+	bar := pb.ProgressBarTemplate(`{{ red "Talisman Scan:" }} {{counters .}} {{ bar . "<" "-" (cycle . "↖" "↗" "↘" "↙" ) "." ">"}} {{percent . | rndcolor }} {{green}} {{blue}}`).New(total)
+	bar.Set(pb.Terminal, true)
+	d.bar = bar.Start()
+}
+
+func (d *defaultProgressBar) Increment() {
+	d.bar.Increment()
+}
+
+func (d *defaultProgressBar) Finish() {
+	d.bar.Finish()
 }
