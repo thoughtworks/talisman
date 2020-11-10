ea2c0c01941324e51c3e15f358a1bb36bc413117
Suhas Vishwanath <suhas.vishwanath@thoughtworks.com>
2020-11-10 23:37:57 +0530

    Use TalismanRC in scan mode. #274

--- build_binaries.bash
+++ build_binaries.bash
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 docker image build --rm -t build_talisman .
 docker container run --rm -v `pwd`:/go/src/github.com/thoughtworks/talisman -it --name build_talisman build_talisman
 # need to find a good way to make sure that the binaries and the checksum file are not owned by root
