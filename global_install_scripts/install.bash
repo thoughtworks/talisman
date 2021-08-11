94c7e3a93a7a5e47a48bf652b9e5d6cc3f54a27a
Suhas Vishwanath <suhas.vishwanath@thoughtworks.com>
2021-08-11 06:54:56 +0530

    Reverting M1 mac support till bug in refactored code is fixed

--- global_install_scripts/install.bash
+++ global_install_scripts/install.bash
@@ -113,9 +113,9 @@ function run() {
 		"i686" | "i386")
 			SUFFIX="${SUFFIX}_386"
 			;;
-		"arm64")
-			SUFFIX="${SUFFIX}_arm64"
-			;;
+		#"arm64")
+		#	SUFFIX="${SUFFIX}_arm64"
+		#	;;
 		*)
 			echo_error "Talisman currently only supports x86 and x86_64 architectures."
 			echo_error "If this is a problem for you, please open an issue: https://github.com/${INSTALL_ORG_REPO}/issues/new"
