ada02d9c27ac36a5c1e81c61ed4154428e262b19
Suhas Vishwanath <suhas.vishwanath@thoughtworks.com>
2022-07-04 01:15:20 +0530

    Update scopes.go

--- talismanrc/scopes.go
+++ talismanrc/scopes.go
@@ -2,7 +2,7 @@ package talismanrc
 
 var knownScopes = map[string][]string{
 	"node":      {"yarn.lock", "package-lock.json", "node_modules/"},
-	"go":        {"makefile", "go.mod", "go.sum", "Gopkg.toml", "Gopkg.lock", "glide.yaml", "glide.lock", "vendor/"},
+	"go":        {"makefile", "go.mod", "go.sum", "Gopkg.toml", "Gopkg.lock", "glide.yaml", "glide.lock"},
 	"images":    {"*.jpeg", "*.jpg", "*.png", "*.tiff", "*.bmp"},
 	"bazel":     {"*.bzl"},
 	"terraform": {".terraform.lock.hcl"},
