fe4c7660114b3b8a2e98cf7de43564b02a82b1a6
Javier L. Matías-Cabrera <jmatias@thoughtworks.com>
2022-07-11 19:37:23 -0400

    Code formatting. (#376)

--- global_install_scripts/add_to_talismanignore_in_git_repo.bash
+++ global_install_scripts/add_to_talismanignore_in_git_repo.bash
@@ -5,17 +5,17 @@ GIT_REPO_DOT_GIT=$1
 IGNORE_PATTERN=$2
 
 function echo_debug() {
-    [[ -z "${DEBUG}" ]] && return
-    echo -ne $(tput setaf 3) >&2
-    echo "$1" >&2
-    echo -ne $(tput sgr0) >&2
+  [[ -z "${DEBUG}" ]] && return
+  echo -ne $(tput setaf 3) >&2
+  echo "$1" >&2
+  echo -ne $(tput sgr0) >&2
 }
-    
+
 function echo_success {
-    echo -ne $(tput setaf 2)
-    echo "$1" >&2
-    echo -ne $(tput sgr0)
+  echo -ne $(tput setaf 2)
+  echo "$1" >&2
+  echo -ne $(tput sgr0)
 }
 
 echo_debug "Adding $IGNORE_PATTERN to ${GIT_REPO_DOT_GIT}/../.talismanignore"
-echo $IGNORE_PATTERN >> ${GIT_REPO_DOT_GIT}/../.talismanignore && echo_success "Added $IGNORE_PATTERN to ${GIT_REPO_DOT_GIT}/../.talismanignore"
+echo $IGNORE_PATTERN >>${GIT_REPO_DOT_GIT}/../.talismanignore && echo_success "Added $IGNORE_PATTERN to ${GIT_REPO_DOT_GIT}/../.talismanignore"
