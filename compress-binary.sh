6eaf63fdfaf21ecc257c25ec23502a75b5e51c34
Javier Matias-Cabrera <jmatias@thoughtworks.com>
2022-09-19 20:08:39 -0400

    Compress talisman binaries.

---
+++ compress-binary.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd dist || exit
+
+binary=$1
+
+if [[ $binary == *"arm"* ]]; then
+  exit 0
+fi
+
+echo "Compressing binaries"
+upx --lzma "$binary"
+echo "...Done"
+
+echo "Verifying compressed binaries"
+upx -t "$binary"
+echo "...Done"
+
+popd || exit
