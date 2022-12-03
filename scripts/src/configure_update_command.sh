01ab20953c60597ced359d011c466a3619a7fe17
Javier Matias-Cabrera <jmatias@thoughtworks.com>
2022-12-02 23:12:58 -0500

    Added \'cli configure update\' command.

---
+++ scripts/src/configure_update_command.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+repo_org="${args["--repo-org"]}"
+binary_name="talisman_$(__os_name)_$(__arch)"
+
+assets=$(curl -Ls https://api.github.com/repos/"$repo_org"/releases/latest)
+latest_version=$(echo "$assets" | grep tag_name | awk '{print $2}' | tr -d '"' | tr -d ',' | tr -d 'v')
+current_version=$(talisman --version | awk '{print $2}')
+
+
+if [[ "$latest_version" != "$current_version" ]]; then
+    $(source_dir)/cli configure download
+fi
