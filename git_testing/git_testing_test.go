1b6f393be707d37df592edeeeaa9b2188167d899
Owen Nelson <owen.nelson@thoughtworks.com>
2025-04-18 16:09:10 -0500

    Test GitTesting through wrapper function
    Authored-by: Owen Nelson <owen.nelson@thoughtworks.com>

--- git_testing/git_testing_test.go
+++ git_testing/git_testing_test.go
@@ -24,73 +24,76 @@ func init() {
 }
 
 func TestInitializingANewRepoSetsUpFolderAndGitStructures(t *testing.T) {
-	repo := Init()
-	defer repo.Clean()
-	assert.True(t, exists(repo.gitRoot), "GitTesting initialization should create the directory structure required")
-	assert.True(t, isGitRepo(repo.gitRoot), "Repo root does not contain the .git folder")
+	DoInTempGitRepo(func(repo *GitTesting) {
+		assert.True(t, exists(repo.gitRoot), "GitTesting initialization should create the directory structure required")
+		assert.True(t, isGitRepo(repo.gitRoot), "Repo root does not contain the .git folder")
+	})
 }
 
 func TestSettingUpBaselineFilesSetsUpACommitInRepo(t *testing.T) {
-	repo := Init()
-	defer repo.Clean()
-	repo.SetupBaselineFiles("a.txt", filepath.Join("alice", "bob", "b.txt"))
-	verifyPresenceOfGitRepoWithCommits(t, 1, repo.gitRoot)
+	DoInTempGitRepo(func(repo *GitTesting) {
+		repo.SetupBaselineFiles("a.txt", filepath.Join("alice", "bob", "b.txt"))
+		verifyPresenceOfGitRepoWithCommits(t, 1, repo.gitRoot)
+	})
 }
 
 func TestEditingFilesInARepoWorks(t *testing.T) {
-	repo := Init()
-	defer repo.Clean()
-	repo.SetupBaselineFiles("a.txt", filepath.Join("alice", "bob", "b.txt"))
-	repo.AppendFileContent("a.txt", "\nmonkey see.\n", "monkey do.")
-	content := repo.FileContents("a.txt")
-	assert.True(t, strings.HasSuffix(string(content), "monkey see.\nmonkey do."))
-	repo.AddAndcommit("a.txt", "modified content")
-	verifyPresenceOfGitRepoWithCommits(t, 2, repo.gitRoot)
+	DoInTempGitRepo(func(repo *GitTesting) {
+		repo.SetupBaselineFiles("a.txt", filepath.Join("alice", "bob", "b.txt"))
+		repo.AppendFileContent("a.txt", "\nmonkey see.\n", "monkey do.")
+		content := repo.FileContents("a.txt")
+		assert.True(t, strings.HasSuffix(string(content), "monkey see.\nmonkey do."))
+		repo.AddAndcommit("a.txt", "modified content")
+		verifyPresenceOfGitRepoWithCommits(t, 2, repo.gitRoot)
+	})
 }
 
 func TestRemovingFilesInARepoWorks(t *testing.T) {
-	repo := Init()
-	defer repo.Clean()
-	repo.SetupBaselineFiles("a.txt", filepath.Join("alice", "bob", "b.txt"))
-	repo.RemoveFile("a.txt")
-	assert.False(t, exists(filepath.Join("data", "testLocation1", "a.txt")), "Unexpected. Deleted file a.txt still exists inside the repo")
-	repo.AddAndcommit("a.txt", "removed it")
-	verifyPresenceOfGitRepoWithCommits(t, 2, repo.gitRoot)
+	DoInTempGitRepo(func(repo *GitTesting) {
+		repo.SetupBaselineFiles("a.txt", filepath.Join("alice", "bob", "b.txt"))
+		repo.RemoveFile("a.txt")
+		assert.False(t, exists(filepath.Join("data", "testLocation1", "a.txt")), "Unexpected. Deleted file a.txt still exists inside the repo")
+		repo.AddAndcommit("a.txt", "removed it")
+		verifyPresenceOfGitRepoWithCommits(t, 2, repo.gitRoot)
+	})
 }
 
 func TestCloningARepoToAnotherWorks(t *testing.T) {
-	repo := Init()
-	defer repo.Clean()
-	repo.SetupBaselineFiles("a.txt", filepath.Join("alice", "bob", "b.txt"))
 	fs := afero.NewMemMapFs()
 	anotherRepoLocation, _ := afero.TempDir(fs, afero.GetTempDir(fs, "talisman-clone-test"), "")
-	cloned := repo.GitClone(anotherRepoLocation)
-	defer cloned.Clean()
-	verifyPresenceOfGitRepoWithCommits(t, 1, repo.gitRoot)
-	logger.Debug("Finished with first verification")
-	logger.Debugf("Another location is %s\n", anotherRepoLocation)
+	var cloned *GitTesting
+	DoInTempGitRepo(func(repo *GitTesting) {
+		repo.SetupBaselineFiles("a.txt", filepath.Join("alice", "bob", "b.txt"))
+		verifyPresenceOfGitRepoWithCommits(t, 1, repo.gitRoot)
+		logger.Debug("Finished with first verification")
+
+		logger.Debugf("Another location is %s\n", anotherRepoLocation)
+		cloned = repo.GitClone(anotherRepoLocation)
+		repo.Clean()
+	})
 	verifyPresenceOfGitRepoWithCommits(t, 1, anotherRepoLocation)
+	cloned.Clean()
 }
 
 func TestEarliestCommits(t *testing.T) {
-	repo := Init()
-	defer repo.Clean()
-	repo.SetupBaselineFiles("a.txt")
-	initialCommit := repo.EarliestCommit()
-	repo.AppendFileContent("a.txt", "\nmonkey see.\n", "monkey do.")
-	repo.AddAndcommit("a.txt", "modified content")
-	assert.Equal(t, initialCommit, repo.EarliestCommit(), "First commit is not expected to change on repo modifications")
+	DoInTempGitRepo(func(repo *GitTesting) {
+		repo.SetupBaselineFiles("a.txt")
+		initialCommit := repo.EarliestCommit()
+		repo.AppendFileContent("a.txt", "\nmonkey see.\n", "monkey do.")
+		repo.AddAndcommit("a.txt", "modified content")
+		assert.Equal(t, initialCommit, repo.EarliestCommit(), "First commit is not expected to change on repo modifications")
+	})
 }
 
 func TestLatestCommits(t *testing.T) {
-	repo := Init()
-	defer repo.Clean()
-	repo.SetupBaselineFiles("a.txt")
-	repo.AppendFileContent("a.txt", "\nmonkey see.\n", "monkey do.")
-	repo.AddAndcommit("a.txt", "modified content")
-	repo.AppendFileContent("a.txt", "\nline n-1.\n", "line n.")
-	repo.AddAndcommit("a.txt", "more modified content")
-	assert.NotEqual(t, repo.EarliestCommit(), repo.LatestCommit()) //bad test.
+	DoInTempGitRepo(func(repo *GitTesting) {
+		repo.SetupBaselineFiles("a.txt")
+		repo.AppendFileContent("a.txt", "\nmonkey see.\n", "monkey do.")
+		repo.AddAndcommit("a.txt", "modified content")
+		repo.AppendFileContent("a.txt", "\nline n-1.\n", "line n.")
+		repo.AddAndcommit("a.txt", "more modified content")
+		assert.NotEqual(t, repo.EarliestCommit(), repo.LatestCommit()) //bad test.
+	})
 }
 
 func verifyPresenceOfGitRepoWithCommits(t *testing.T, expectedCommitCount int, repoLocation string) {
